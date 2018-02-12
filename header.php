bcf9e2a6347f68730a6eb026029686553b64023b
CorentinCrz <ccroiz@live.fr>
2018-02-12 15:13:33 +0100

    update style.css, header.php, add img

--- header.php
+++ header.php
@@ -2,13 +2,30 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="reset.css">
-    <title></title>
+    <link rel="stylesheet" href="css/reset.css" type="text/css">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+    <title>header</title>
   </head>
+
   <body>
     <header class="header">
+      <section class="headerUp">
+        <h1 class="headerTitle">TUTURE</h1>
+        <ul class="headerList">
+          <li class="headerListItems"><img class="headerIcon" src="img/user.png" alt="user icon">Se connecter</li>
+          <li class="headerListItems"><img class="headerIcon" src="img/like.png" alt="like icon">Favoris</li>
+        </ul>
+      </section>
+      <section class="headerDown">
+        <ul class="headerList">
+          <li class="headerListItems">Acceuil</li>
+          <li class="headerListItems">Déposer une annonce</li>
+          <li class="headerListItems">Offres</li>
+          <li class="headerListItems">Fiches techniques</li>
+          <li class="headerListItems">Annuaire des professionnels</li>
+        </ul>
 
+      </section>
     </header>
 
   </body>
